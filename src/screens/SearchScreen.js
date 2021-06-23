10d76e6ffeb12f90a31300d4731991786db67ebf
MaryAnnN28 <maryann.navarrete@gmail.com>
2021-06-22 21:01:45 -0400

    added click on image to navigate to detailed result

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -5,6 +5,7 @@ import useResults from '../hooks/useResults';
 import ResultsList from '../components/ResultsList';
 
 const SearchScreen = () => {
+
   const [term, setTerm] = useState('');
   const [searchApi, results, errorMessage] = useResults();
   
@@ -15,21 +16,26 @@ const SearchScreen = () => {
   }; 
 
   return (
-    <View style={{ flex: 1 }}>
+    <>
       <SearchBar
         term={term}
         onTermChange={setTerm}
         onTermSubmit={() => searchApi(term)}
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
-      <Text>We have found {results.length} results</Text>
+    
       <ScrollView>
-        <ResultsList results={filterResultsByPrice('$')} title="Cost Effective" />
-        <ResultsList results={filterResultsByPrice('$$')} title="Bit Pricier" />
-        <ResultsList results={filterResultsByPrice('$$$')} title="Big Spender!" />
-        <ResultsList results={filterResultsByPrice('$$$$')} title="Wow!" />
+        <ResultsList
+          results={filterResultsByPrice('$')}
+          title="Cost Effective" />
+        <ResultsList
+          results={filterResultsByPrice('$$')}
+          title="Bit Pricier" />
+        <ResultsList
+          results={filterResultsByPrice('$$$')}
+          title="Big Spender!" />
       </ScrollView>
-    </View>
+    </>
   )
 };
 
