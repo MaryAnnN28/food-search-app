5b75e78564b8e2cf35d7f2419de729014aa23981
MaryAnnN28 <maryann.navarrete@gmail.com>
2021-06-26 15:11:01 -0400

    added more details to results show page

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -26,9 +26,25 @@ const ResultsShowScreen = ({ navigation }) => {
   return (
     <View style={styles.pageStyle}>
       <Text style={styles.header}>{result.name}</Text>
-      <Text>Rating:{result.rating}</Text>
-      <Text>Reviews: {result.review_count}</Text>
-      <Text>Phone: {result.display_phone}</Text>
+      <Text style={styles.info}>Rating: {result.rating}</Text>
+      <Text style={styles.info}>Price: {result.price}</Text>
+      <Text style={styles.info}>Reviews: {result.review_count}</Text>
+      <Text style={styles.info}>Phone: {result.display_phone}</Text>
+      <View>
+        <Text style={styles.info}>
+          Services: 
+        </Text>
+        <FlatList
+          // horizontal
+          data={result.transactions}
+          style={styles.infoList}
+          keyExtractor={(transaction) => transaction}
+          renderItem={({ item }) => {
+            return <Text style={styles.infoDetails}>{item}</Text>
+          }}
+        />
+
+      </View>
       <FlatList
         data={result.photos}
         keyExtractor={(photo) => photo}
@@ -44,18 +60,30 @@ const styles = StyleSheet.create({
   pageStyle: {
     marginTop: 5,
     justifyContent: 'center',
-    flexDirection: 'column',
-    alignItems: 'center'
+    flexDirection: 'column'
   }, 
   image: {
     height: 200,
     width: 300,
-    marginVertical: 5, 
+    marginVertical: 5,
+    alignSelf: 'center'
   },
   header: {
     fontWeight: 'bold',
     fontSize: 18,
-    marginBottom: 10
+    marginBottom: 10,
+    alignSelf: 'center'
+  },
+  info: {
+    textAlign: 'left',
+    marginLeft: 70
+  },
+  infoList: {
+    marginLeft: 70,
+    flexDirection: 'column'
+  },
+  infoDetails: {
+    paddingHorizontal: 70
   }
 });
 
